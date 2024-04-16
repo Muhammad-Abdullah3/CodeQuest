ee5f12ed3a542ea49e56979a7273c378bc0b9a59
Muhammad-Abdullah3 <maabdullah.rhs@gmail.com>
2024-04-16 06:56:14 +0500

    modified:   Java Quest/Projects/E Voting System/src/Login.java

--- Java Quest/Projects/E Voting System/src/Login.java
+++ Java Quest/Projects/E Voting System/src/Login.java
@@ -1,18 +1,38 @@
 import java.util.Scanner;
-class Signup 
+class LogIn 
 {
-	int choice = 0;
+	int choice = 0; String searchName,searchCnic;
 	Scanner inp = new Scanner(System.in);
 	public void candLogIn()
 	{
 		Candidate can = new Candidate();
-		System.out.println("Congratulations You have Signed up as a Candidate.");
+		System.out.println("Enter Your Name: ");
+		searchName = inp.nextLine();
+		searchCnic = inp.nextLine();
+		for(int i=0;i<Candidate.canName.size();i++)
+		{
+			if(Candidate.canName.get(i)==searchName&&Candidate.canCnic.get(i)==searchCnic)
+			{
+				System.out.print("Login Successful");
+				
+			}
+		}
 	}
 	public void voterLogIn()
 	{
 		Voter vo = new Voter();
+		System.out.println("Enter Your Name: ");
+		searchName = inp.nextLine();
+		searchCnic = inp.nextLine();
+		for(int i=0;i<Voter.votName.size();i++)
+		{
+			if(Voter.votName.get(i)==searchName&&Voter.votCnic.get(i)==searchCnic)
+			{
+				System.out.print("Login Successful for Voter.");
+			}
+		}
 	}
-	public void menu()
+	public void LogInmenu()
 	{
 		System.out.println("Press 1 for Signing Up as a Candidate:");
 		System.out.println("Press 2 for Signing Up as a Voter:");
